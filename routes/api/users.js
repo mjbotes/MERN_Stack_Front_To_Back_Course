1def5873f5604c307291c0edbdcdaecfe6e526dd
Marnus BOTES <mbotes@c4r21s1.wethinkcode.co.za>
2019-12-02 11:17:39 +0200

    Test

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator/check');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
+const User = require('../../models/User');
 
 //@route    POST api/users
 //@desc     Register User
@@ -22,8 +26,48 @@ router.post(
 		if (!errors.isEmpty()) {
 			return res.status(400).json({ errors: errors.array() });
 		}
+		const { name, email, password } = req.bodu;
+		try {
+			// See if user Exists
 
-		res.send('Users Route');
+			let user = User.findOne({ email });
+
+			if (user) {
+				res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+			}
+
+			// Get users Gravatar
+
+			const avatar = gravatar.url(email, {
+				s: '200',
+				r: 'pg',
+				d: 'mm'
+			});
+
+			user = new User({
+				name,
+				email,
+				avatar,
+				password
+			});
+
+			// Encrypt Password
+
+			const salt = bcrypt.genSalt(10);
+
+			user.password = bcrypt.hash(password, salt);
+
+			// Save user to MongoDB
+
+			await user.save();
+
+			// Return jsonwebtokem
+
+			res.send('Users Route');
+		} catch (err) {
+			console.log(err.message);
+			res.status(500).send('Server Error!!');
+		}
 	}
 );
 
