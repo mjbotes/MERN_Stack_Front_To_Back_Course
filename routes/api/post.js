6a1ef08f5a450d16746f68d1375954b533618197
Marnus BOTES <mbotes@c4r21s1.wethinkcode.co.za>
2019-12-04 16:10:40 +0200

    Finished Backend and made Template for Frontend

--- routes/api/post.js
+++ routes/api/post.js
@@ -7,7 +7,7 @@ const Profile = require('../../models/Profile');
 const User = require('../../models/User');
 const Post = require('../../models/Post');
 
-//@route    GET api/post
+//@route    Post api/post
 //@desc     Add post
 //@acess    Private
 router.post(
@@ -105,6 +105,160 @@ router.delete('/:id', auth, async (req, res) => {
 		}
 		console.error(err.message);
 		res.status(500).send('server Error');
+	}
+});
+
+//@route    PUT api/post/like/:id
+//@desc     like a post
+//@acess    Private
+router.put('/like/:id', auth, async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+
+		if (!post) {
+			return res.status(401).send('No such Post');
+		}
+
+		// check if already been liked
+		if (
+			post.likes.filter(like => like.user.toString() === req.user.id).length > 0
+		) {
+			return res.status(400).json({ msg: 'Post was already liked' });
+		}
+		post.likes.unshift({ user: req.user.id });
+
+		await post.save();
+
+		return res.json(post.likes);
+	} catch (err) {
+		if (err.kind === 'objectId') {
+			return res.status(401).send('No such Post');
+		}
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
+});
+
+//@route    PUT api/post/unlike/:id
+//@desc     unlike a post
+//@acess    Private
+router.put('/unlike/:id', auth, async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+
+		if (!post) {
+			return res.status(401).send('No such Post');
+		}
+
+		// check if already been liked
+		if (
+			post.likes.filter(like => like.user.toString() === req.user.id).length ===
+			0
+		) {
+			return res.status(400).json({ msg: 'Post has not been liked' });
+		}
+		// get remove index
+		const removeIndex = post.likes
+			.map(like => like.user.toString())
+			.indexOf(req.user.id);
+
+		post.likes.splice(removeIndex, 1);
+
+		await post.save();
+
+		return res.json(post.likes);
+	} catch (err) {
+		if (err.kind === 'objectId') {
+			return res.status(401).send('No such Post');
+		}
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
+});
+
+//@route    Post api/post/comment/:id
+//@desc     Add comment to post
+//@acess    Private
+router.post(
+	'/comment/:id',
+	[
+		auth,
+		[
+			check('text', 'text is required')
+				.not()
+				.isEmpty()
+		]
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+		try {
+			const user = await User.findById(req.user.id).select('-password');
+			const post = await Post.findById(req.params.id);
+
+			if (!post) {
+				return res.status(401).send({ msg: 'No such Post' });
+			}
+
+			const newComment = new Post({
+				text: req.body.text,
+				name: user.name,
+				avatar: user.avatar,
+				user: req.user.id
+			});
+
+			post.comments.unshift(newComment);
+
+			await post.save();
+
+			return res.json(post.comments);
+		} catch (err) {
+			if (err.kind === 'ObjectId') {
+				return res.status(401).send('No such Post');
+			}
+			console.error(err.message);
+			return res.status(500).send('Server Error');
+		}
+	}
+);
+
+//@route    DELETE api/post/comment/:id/:c_id
+//@desc     Delete comment from post
+//@acess    Private
+router.delete('/comment/:id/:c_id', auth, async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+
+		if (!post) {
+			return res.status(401).send({ msg: 'No such Post' });
+		}
+
+		const comment = post.comments.find(
+			comment => comment.id === req.params.c_id
+		);
+
+		if (!comment) {
+			return res.status(401).send({ msg: 'No such Comment on this Post' });
+		}
+
+		if (comment.user.toString() !== req.user.id) {
+			return res.status(401).send({ msg: 'User Not authorised' });
+		}
+
+		const removeIndex = post.comments
+			.map(comment => comment.id)
+			.indexOf(req.params.c_id);
+
+		post.comments.splice(removeIndex, 1);
+
+		await post.save();
+
+		return res.json(post.comments);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error');
 	}
 });
 
