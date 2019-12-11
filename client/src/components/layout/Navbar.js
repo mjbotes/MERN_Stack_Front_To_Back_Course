8c46460201988bd52a3c06230cd2868dd9f567c6
mjbotes <mbotes@student.wethinkcode.co.za>
2019-12-12 00:00:00 +0200

    Finished

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,53 +1,52 @@
 import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { logout } from '../../actions/auth';
 import PropTypes from 'prop-types';
+import { logout } from '../../actions/auth';
 
 const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
 	const authLinks = (
 		<ul>
 			<li>
+				<Link to='/profiles'>Developers</Link>
+			</li>
+			<li>
 				<Link to='/posts'>Posts</Link>
 			</li>
 			<li>
 				<Link to='/dashboard'>
-					<i className='fas fa-user'></i>{' '}
+					<i className='fas fa-user' />{' '}
 					<span className='hide-sm'>Dashboard</span>
 				</Link>
 			</li>
 			<li>
-				<Link to='/profiles'>
-					<i className='fas fa-users'></i>{' '}
-					<span className='hide-sm'>Developers</span>
-				</Link>
-			</li>
-			<li>
 				<a onClick={logout} href='#!'>
-					<i className='fas fa-sign-out-alt'></i>{' '}
+					<i className='fas fa-sign-out-alt' />{' '}
 					<span className='hide-sm'>Logout</span>
 				</a>
 			</li>
 		</ul>
 	);
+
 	const guestLinks = (
 		<ul>
 			<li>
 				<Link to='/profiles'>Developers</Link>
 			</li>
 			<li>
-				<Link to='/register'>register</Link>
+				<Link to='/register'>Register</Link>
 			</li>
 			<li>
 				<Link to='/login'>Login</Link>
 			</li>
 		</ul>
 	);
+
 	return (
 		<nav className='navbar bg-dark'>
 			<h1>
-				<Link to='/#!'>
-					<i className='fas fa-code'></i> DevConnector{' '}
+				<Link to='/'>
+					<i className='fas fa-code' /> DevConnector
 				</Link>
 			</h1>
 			{!loading && (
@@ -57,7 +56,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
 	);
 };
 
-Navbar.protoTypes = {
+Navbar.propTypes = {
 	logout: PropTypes.func.isRequired,
 	auth: PropTypes.object.isRequired
 };
